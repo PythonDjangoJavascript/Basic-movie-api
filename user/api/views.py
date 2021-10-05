9c80b50d01abda85e015a76240aa5b8fc00010c5
Sayed <nuruddinsayeed@gmail.com>
2021-10-05 23:30:09 +0600

    Token generation post_save singal added and used in registration response

--- user/api/views.py
+++ user/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,8 +15,16 @@ def user_registration_view(request):
 
         print(request.data)
         if serialized_data.is_valid():
-            serialized_data.save()
-            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+            user = serialized_data.save()
+            # token = Token.objects.get_or_create(user=user)
+            # print(f'------------------{token}--------------')
+            # print(f'------------------{token[0].key}--------------')
+            data = {
+                **serialized_data.data,
+                'Token': Token.objects.get(user=user).key
+            }
+
+            return Response(data, status=status.HTTP_201_CREATED)
 
         return Response({'Error': serialized_data.errors, },
                         status=status.HTTP_400_BAD_REQUEST)
--- user/apps.py
+++ user/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
+
+    def ready(self) -> None:
+        """Call all signals to execute"""
+        import user.api.signals
